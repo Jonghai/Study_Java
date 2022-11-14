2464b206c0bad70ff736698deb467210168697d6
JongHa11 <ljh7859@clipsoft.co.kr>
2022-11-14 14:48:33 +0900

    IOstudy3.java//파일병합 기능

--- Work03/src/work03/IOstudy3.java
+++ Work03/src/work03/IOstudy3.java
@@ -4,37 +4,42 @@ import java.io.*;
 
 public class IOstudy3 {
     public static void main(String[] args) throws IOException {
-        try {
             String path = "C:\\testfolder\\";
             String fileNm = "mergeFile.txt";
             File mergeFile = new File(path + "mergefolder\\" + fileNm);
             File folder = new File(path);
             File[] fileList = folder.listFiles();
-
+            BufferedReader input = null;
+            BufferedOutputStream output = null;
+            String line = "";
+            byte[] data = null;
+        try {
+            output = new BufferedOutputStream(new FileOutputStream(mergeFile));
             for (File file : fileList) {
                 if (file.exists() && file.isFile()) {
-                    FileReader fileReader = new FileReader(file);
-                    BufferedReader bufferedReader = new BufferedReader(fileReader);
-                    String line = "";
-                    String result = "";
-                    while ((line = bufferedReader.readLine()) != null) {
-                        result += line + "\n";
+                    input = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+                    while ((line = input.readLine()) != null) {
+                        data = line.getBytes();
+                        output.write(data);
+                        output.write(System.lineSeparator().getBytes());
                     }
-                    //System.out.println(result);
-
-                    FileWriter fileWriter = new FileWriter(mergeFile);
-                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                    if (!result.isEmpty()) {
-                        bufferedWriter.write(result);
-                    }
-                    System.out.println(result);
-                    fileReader.close();
-                    bufferedReader.close();
-                    bufferedWriter.close();
                 }
             }
-        }catch (Exception e){
-            e.printStackTrace();
+        }finally {
+            if(input != null){
+                try {
+                    input.close();
+                }catch (Exception ignore){
+                    System.out.println("ignore: {} "+ignore);
+                }
+            }
+            if(output !=null){
+                try {
+                    output.close();;
+                }catch (Exception ignore){
+                    System.out.println("ignore: {} "+ignore);
+                }
+            }
         }
     }
 }
