2d6efe99b952f2d8a5157a6a4411c0fd06090c40
JongHa11 <ljh7859@clipsoft.co.kr>
2022-11-23 17:43:18 +0900

    IOstudy4.java//파일병합 수정중

--- Work03/src/work03/IOstudy4.java
+++ Work03/src/work03/IOstudy4.java
@@ -1,46 +1,75 @@
 package work03;
 
-import java.io.*;
+//어떤 디렉토리 경로를 입력 받아서 그 내부에 있는 모든 파일을 하나로 합쳐서 단 하나의 파일로 만드는 메서드.
+//위에 메서드와 반대로 하나로 합쳐진 파일을 다시 풀어서 각각의 원본 파일로 복구하는 메서드
 
-/*class fileinfo{
-    String fileNm;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+
+class FileInfo{
+    String fileName;
     long fileLength;
 
-    public void setFile(){
+    public JSONArray setFile() throws IOException {
         IOstudy4 io = new IOstudy4();
+        JSONArray array = new JSONArray();
         for(File file : io.files()){
-
+            JSONObject json = new JSONObject();
+            fileName = file.getName();
+            fileLength = file.length();
+            json.put("fileName",fileName);
+            json.put("fileLength",fileLength);
+            array.put(json);
         }
+        //System.out.println(array.opt(3));
+        return array;
 
     }
 
-}*/
+}
 
 public class IOstudy4 {
      String path = "C:\\testfolder\\";
      String fileNm = "mergeFile.txt";
 
-    public File[] files() throws IOException {
+    public File[] files() {
         File folder = new File(path);
         File[] fileList = folder.listFiles();
         return fileList;
     }
 
-    public void data() {
-
-    }
-
     public void mergefile(){
         BufferedReader fis = null;
         BufferedOutputStream fos = null;
-
         try {
+            FileInfo fileinfo = new FileInfo();
             File mergeFile = new File("C:\\mergefolder\\" + fileNm);
             byte[] buffer = null;
             String line = "";
             fos = new BufferedOutputStream(new FileOutputStream(mergeFile));
+            JSONArray json = new JSONArray(fileinfo.setFile());
+            int i = 0;
             for(File file : files()){
-                fos.write((file.getName()+file.length()+"\n").getBytes());
+                JSONObject object = json.optJSONObject(i);
+                i++;
+                //object.toString();
+                byte[] ByteArray = object.toString().getBytes("UTF-8");
+                System.out.println(ByteArray);
+                String bytestring = new String(ByteArray);
+                System.out.println(bytestring);
+
+                //BigInteger n = new BigInteger(new String(ByteArray, "UTF-8"));
+                //byte[] bytes = ByteBuffer.allocate(4).putInt(ByteArray).array();
+               /* String fileName = object.optString("fileName");
+                Long fileLength = object.optLong("fileLength");
+                fos.write(((fileName+fileLength)+"\n").getBytes());*/
+
                 if(file.exists() && file.isFile()){
                     fis = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
                 }
@@ -50,7 +79,7 @@ public class IOstudy4 {
                     fos.write(System.lineSeparator().getBytes());
                 }
             }
-            System.out.println("완료");
+            System.out.println("파일병합완료");
         }catch (Exception e){
             e.printStackTrace();
         }finally {
@@ -61,11 +90,25 @@ public class IOstudy4 {
         }
     }
 
+    /*public void restorefile() throws IOException {
+        FileInfo fileInfo = new FileInfo();
+        File mergefile = new File("C:\\mergefolder\\" + fileNm);
+        BufferedReader fis = null;
+        String line = null;
+        if(mergefile.exists() && mergefile.isFile()){
+            fis = new BufferedReader(new InputStreamReader(new FileInputStream(mergefile)));
+
+        }
+        while ((line = fis.readLine()) != null){
+            if(fileInfo.fileName+fileInfo.fileLength == Line)
+        }
+    }*/
+
     public static void main(String[] args) throws IOException {
         IOstudy4 io = new IOstudy4();
-        //fileinfo finfo = new fileinfo();
+        //FileInfo fileInfo = new FileInfo();
+        //fileInfo.setFile();
         io.mergefile();
-        //finfo.setFile();
-        //io.files();
+        //io.restorefile();
     }
 }
